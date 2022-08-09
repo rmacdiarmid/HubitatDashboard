87adf8056a994734104f329e6142b687e45546c1
Ryan MacDiarmid <ryanmacdiarmid@gmail.com>
2022-08-09 12:24:13 -0700

    Mockup for homeautomation page

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rmacdiarmid/hubitatdashboard
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+//Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+//About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	fmt.Fprint(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+}
+
+//addValues adds two integers and returns the sum
+func addValues(x, y int) int {
+	return x + y
+}
+
+func main() {
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
+
+}
