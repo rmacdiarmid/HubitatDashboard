6e2c58d6e67ab3cf603da1181f3b032e430ceb3a
Ryan MacDiarmid <ryanmacdiarmid@gmail.com>
2022-08-09 15:35:36 -0700

    brok e out render.go

--- handlers.go
+++ handlers.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
 	"net/http"
 )
 
@@ -14,13 +12,4 @@ func Home(w http.ResponseWriter, r *http.Request) {
 //About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
 
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template:", err)
-		return
-	}
 }
