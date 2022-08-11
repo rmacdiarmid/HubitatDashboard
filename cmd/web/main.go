82cc0d481c8ed80c9635bbd3b5276780329ee117
Ryan MacDiarmid <ryanmacdiarmid@gmail.com>
2022-08-11 13:47:32 -0700

    L31 - Got it finally working
    I missed a freaking / on line 52 of render page

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,15 +7,13 @@ import (
 	"github.com/rmacdiarmid/HubitatDashboard/pkg/handlers"
 )
 
-//port Number
 const portNumber = ":8080"
 
+// main is the main function
 func main() {
-
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
-	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
-
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,20 +1,22 @@
 package render
 
 import (
-	"fmt"
+	"bytes"
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
-/* //Renders Templates using HTML templates
+// RenderTemplate renders a template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	//create a template cache
+	// create a template cache
 	tc, err := createTemplateCache()
 	if err != nil {
 		log.Fatal(err)
 	}
-	//get requested template from cache
+
+	// get requested template from cache
 	t, ok := tc[tmpl]
 	if !ok {
 		log.Fatal(err)
@@ -24,24 +26,25 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 
 	err = t.Execute(buf, nil)
 	if err != nil {
-		log.Print(err)
+		log.Println(err)
 	}
 
-	//render the template
+	// render the template
 	_, err = buf.WriteTo(w)
 	if err != nil {
-		log.Println("Error writing template to browser", err)
+		log.Println(err)
 	}
 }
 
 func createTemplateCache() (map[string]*template.Template, error) {
 	myCache := map[string]*template.Template{}
 
-	//get all of the files named *.page.tmpl from ./templates
+	// get all of the files named *.page.tmpl from ./templates
 	pages, err := filepath.Glob("./templates/*.page.tmpl")
 	if err != nil {
 		return myCache, err
 	}
+
 	// range through all files ending with *.page.tmpl
 	for _, page := range pages {
 		name := filepath.Base(page)
@@ -49,69 +52,21 @@ func createTemplateCache() (map[string]*template.Template, error) {
 		if err != nil {
 			return myCache, err
 		}
-		matches, err := filepath.Glob(".templates/*.layout.tmpl")
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
 		if err != nil {
 			return myCache, err
 		}
+
 		if len(matches) > 0 {
 			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
 			if err != nil {
 				return myCache, err
 			}
 		}
+
 		myCache[name] = ts
 	}
+
 	return myCache, nil
-} */
-
-func RenderTemplateTest(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template:", err)
-	}
-}
-
-var tc = make(map[string]*template.Template)
-
-func RenderTemplate(w http.ResponseWriter, t string) {
-	var tmpl *template.Template
-	var err error
-
-	//check to see if we already have the template in our cache
-	_, inMap := tc[t]
-	if !inMap {
-		//need ot create the template
-		log.Println("creating template and adding to cache")
-		err = createTemplateCache(t)
-		if err != nil {
-			log.Println(err)
-		}
-	} else {
-		//we have the template in the cache
-		log.Println("using cached template")
-	}
-	tmpl = tc[t]
-	err = tmpl.Execute(w, nil)
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-func createTemplateCache(t string) error {
-	templates := []string{
-		fmt.Sprintf("./templates/%s", t),
-		"./templates/base.layout.tmpl",
-	}
-
-	//parse the template
-	tmpl, err := template.ParseFiles(templates...)
-	if err != nil {
-		return err
-	}
-
-	//add template to cache
-	tc[t] = tmpl
-
-	return nil
 }
