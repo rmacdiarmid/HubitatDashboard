99d25a0befc320c468152679ccef2d277b90b23f
Ryan MacDiarmid <ryanmacdiarmid@gmail.com>
2022-08-11 17:50:48 -0700

    L39 - switched router to Chi added Recoverer, complete working

---
+++ cmd/web/routes.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/rmacdiarmid/HubitatDashboard/pkg/config"
+	"github.com/rmacdiarmid/HubitatDashboard/pkg/handlers"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	/* mux := pat.New()
+	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	mux.Get("/about", http.HandlerFunc(handlers.Repo.About)) */
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+	return mux
+}
